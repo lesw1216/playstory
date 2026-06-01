95f73a47b3e008f035a0aab8c1c7ad95f1223562
sangwoo <lesw1216@gmail.com>
2026-06-01 17:32:48 +0900

    feat: vue 초기 설정
    샘플 코드 제거
    tailwind 추가

--- frontend/src/components/ExampleComponent.vue
+++ frontend/src/components/ExampleComponent.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <div></div>
-  <h1 class="text-3xl font-bold">
-    Hello world!
-  </h1>
+    <div></div>
+    <h1 class="text-3xl font-bold">
+        Hello world!
+    </h1>
 </template>
 
 <style scoped></style>
