461c65e9c5391499f6c5eb4794009482d158d4ec
sangwoo <lesw1216@gmail.com>
2026-06-01 16:35:38 +0900

    feat: DB 스키마 초기 설정
    * schema.sql 생성
    * application.yaml DB 관련 설정 추가

---
+++ backend/src/main/resources/db/schema.sql
@@ -0,0 +1,22 @@
+create table if not exists orders
+(
+    id           integer auto_increment primary key,
+    user_name    varchar(20) not null,
+    product_name varchar(50) not null,
+    category     varchar(10) not null,
+    amount       integer     not null,
+    status       varchar(10) not null,
+    order_date   timestamp   not null
+    );
+
+create table if not exists excel_export_jobs
+(
+    id              integer auto_increment primary key,
+    requested_at    timestamp not null,
+    started_at      timestamp not null,
+    ended_at        timestamp not null,
+    status          varchar(10) not null,
+    file_path       varchar(200) not null
+    );
+
+create index idx_requested_at on excel_export_jobs (requested_at);
